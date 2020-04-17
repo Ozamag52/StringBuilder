e223cff98cc6520ddd0bc0d734dddbe6e497e2e3
Gustavo Peixoto <henriquegustavo2010@gmail.com>
2020-04-17 11:32:16 -0300

    Criacao da classe Comment

---
+++ StringBuilder/Entities/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringBuilder.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; } // Auto proprieties   
+
+
+
+        public Comment() // Construtor padrão da classe 
+        {
+
+        }
+
+        public Comment(string text) // Construtor customizado 
+        {
+            Text = text;
+        }
+    }
+}
