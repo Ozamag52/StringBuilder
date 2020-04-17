c7d0c32368030ca9c3d761592a7e9ff5464b7d91
Gustavo Peixoto <henriquegustavo2010@gmail.com>
2020-04-17 11:56:01 -0300

    Instanciamento das classes Post e Comment no programa principal

--- StringBuilder/Program.cs
+++ StringBuilder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using StringBuilder.Entities;
 
 namespace StringBuilder
 {
@@ -6,7 +7,20 @@ namespace StringBuilder
     {
         static void Main(string[] args)
         {
-            
+            // Instanciando objetos da classe Comment 
+            Comment C1 = new Comment("Have a nice trip");
+            Comment C2 = new Comment("Wow that's awesome!");
+            Comment C3 = new Comment("Good night");
+            Comment C4 = new Comment("May the Force be with you");
+
+            Post P1 = new Post(DateTime.Parse("21/06/2018 13:05:44"), "Traveling to New Zeanland", "I'm going to visit this wonderful coutry!", 12);
+            Post P2 = new Post(DateTime.Parse("28/07/2018 23:14:19"), "Good night guys", "See you tomorrow", 5);
+
+            P1.AddComment(C1);
+            P1.AddComment(C2);
+            P2.AddComment(C3);
+            P2.AddComment(C4);
+
         }
     }
 }
